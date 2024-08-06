ed6558bc09621d2f6e1cc1bd073c5103e17575cb
Mohammed Shekha <msh@odoo.com>
2024-08-06 11:04:54 +0530

    [Solution] 2.1 A new Layout

--- expense_tracker_dashboard/static/src/dashboard.js
+++ expense_tracker_dashboard/static/src/dashboard.js
@@ -1,8 +1,16 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
 
-class AwesomeDashboard extends Component {
-    static template = "awesome_dashboard.AwesomeDashboard";
+class ExpenseDashboard extends Component {
+    static template = "expense_tracker_dashboard.ExpenseDashboard";
+    static components = { Layout };
+
+    setup() {
+        this.display = {
+            controlPanel: {},
+        };
+    }
 }
 
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+registry.category("actions").add("expense_tracker_dashboard.dashboard", ExpenseDashboard);
