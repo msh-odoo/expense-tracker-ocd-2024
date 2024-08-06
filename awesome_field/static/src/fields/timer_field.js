aa31053617dadfd1c46aea42cffbea02ca77a9bd
Mohammed Shekha <msh@odoo.com>
2024-08-06 12:57:12 +0530

    [Solution] 3.2 Add timer field component with helloworld

--- awesome_field/static/src/fields/timer_field.js
+++ awesome_field/static/src/fields/timer_field.js
@@ -1,1 +1,20 @@
-// Code will come here
+import { registry } from "@web/core/registry";
+import { Component } from "@odoo/owl";
+import { _t } from "@web/core/l10n/translation";
+import { standardFieldProps } from "@web/views/fields/standard_field_props";
+
+
+export class TimerField extends Component {
+    static template = "web.TimerField";
+    static props = {
+        ...standardFieldProps,
+    };
+}
+
+export const timerField = {
+    component: TimerField,
+    displayName: _t("Timer"),
+    supportedTypes: ["datetime"],
+}
+
+registry.category("fields").add("timer", timerField);
