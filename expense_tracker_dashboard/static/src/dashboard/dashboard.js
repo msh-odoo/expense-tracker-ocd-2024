ae28d16b267536b8ba838a69cc114f786a3fb5f2
Mohammed Shekha <msh@odoo.com>
2024-08-06 11:54:19 +0530

    [Solution] 2.5 Making our dashboard generic

---
+++ expense_tracker_dashboard/static/src/dashboard/dashboard.js
@@ -0,0 +1,39 @@
+/** @odoo-module **/
+
+import { Component } from "@odoo/owl";
+import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./dashboard_item/dashboard_item";
+import { items } from "./dashboard_items";
+
+class ExpenseDashboard extends Component {
+    static template = "expense_tracker_dashboard.ExpenseDashboard";
+    static components = { Layout, DashboardItem };
+
+    setup() {
+        this.action = useService("action");
+        this.display = {
+            controlPanel: {},
+        };
+        this.items = items;
+    }
+
+    openExpenseView() {
+        this.action.doAction("expense_tracker.action_personal_expense");
+    }
+
+    openCategories() {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "Categories",
+            res_model: "expense.category",
+            views: [
+                [false, "list"],
+                [false, "form"],
+            ],
+        });
+    }
+}
+
+registry.category("lazy_components").add("ExpenseDashboard", ExpenseDashboard);
