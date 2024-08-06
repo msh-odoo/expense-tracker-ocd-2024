69c942dc1d3aada4f1c4996bfda71e36a2dc5f03
Mohammed Shekha <msh@odoo.com>
2024-08-06 11:59:59 +0530

    [Solution] 2.7 Add and remove dashboard items

--- expense_tracker_dashboard/static/src/dashboard/dashboard.js
+++ expense_tracker_dashboard/static/src/dashboard/dashboard.js
@@ -1,10 +1,13 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { DashboardItem } from "./dashboard_item/dashboard_item";
+import { Dialog } from "@web/core/dialog/dialog";
+import { CheckBox } from "@web/core/checkbox/checkbox";
+import { browser } from "@web/core/browser/browser";
 
 class ExpenseDashboard extends Component {
     static template = "expense_tracker_dashboard.ExpenseDashboard";
@@ -12,10 +15,26 @@ class ExpenseDashboard extends Component {
 
     setup() {
         this.action = useService("action");
+        this.dialog = useService("dialog");
         this.display = {
             controlPanel: {},
         };
         this.items = registry.category("awesome_dashboard").getAll();
+        this.state = useState({
+            disabledItems: browser.localStorage.getItem("disabledDashboardItems")?.split(",") || []
+        });
+    }
+
+    openConfiguration() {
+        this.dialog.add(ConfigurationDialog, {
+            items: this.items,
+            disabledItems: this.state.disabledItems,
+            onUpdateConfiguration: this.updateConfiguration.bind(this),
+        })
+    }
+
+    updateConfiguration(newDisabledItems) {
+        this.state.disabledItems = newDisabledItems;
     }
 
     openExpenseView() {
@@ -33,6 +52,40 @@ class ExpenseDashboard extends Component {
             ],
         });
     }
+}
+
+class ConfigurationDialog extends Component {
+    static template = "awesome_dashboard.ConfigurationDialog";
+    static components = { Dialog, CheckBox };
+    static props = ["close", "items", "disabledItems", "onUpdateConfiguration"];
+
+    setup() {
+        this.items = useState(this.props.items.map((item) => {
+            return {
+                ...item,
+                enabled: !this.props.disabledItems.includes(item.id),
+            }
+        }));
+    }
+
+    done() {
+        this.props.close();
+    }
+
+    onChange(checked, changedItem) {
+        changedItem.enabled = checked;
+        const newDisabledItems = Object.values(this.items).filter(
+            (item) => !item.enabled
+        ).map((item) => item.id)
+
+        browser.localStorage.setItem(
+            "disabledDashboardItems",
+            newDisabledItems,
+        );
+
+        this.props.onUpdateConfiguration(newDisabledItems);
+    }
+
 }
 
 registry.category("lazy_components").add("ExpenseDashboard", ExpenseDashboard);
