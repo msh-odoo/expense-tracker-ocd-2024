461c9f2b8b3a8a7c7fdc87547216741b0dc057e8
Mohammed Shekha <msh@odoo.com>
2024-08-06 11:28:52 +0530

    [Solution] 2.2 Add some buttons for quick navigation

--- expense_tracker_dashboard/static/src/dashboard.js
+++ expense_tracker_dashboard/static/src/dashboard.js
@@ -1,15 +1,33 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
 
 class ExpenseDashboard extends Component {
     static template = "expense_tracker_dashboard.ExpenseDashboard";
     static components = { Layout };
 
     setup() {
+        this.action = useService("action");
         this.display = {
             controlPanel: {},
         };
+    }
+
+    openExpenseView() {
+        this.action.doAction("expense_tracker.action_personal_expense");
+    }
+
+    openCategories() {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: "Categories",
+            res_model: "expense.category",
+            views: [
+                [false, "list"],
+                [false, "form"],
+            ],
+        });
     }
 }
 
