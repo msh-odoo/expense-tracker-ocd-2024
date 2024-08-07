1257afb256233e898204b1e10fc5f44bfda5b727
Mohammed Shekha <msh@odoo.com>
2024-08-07 17:32:02 +0530

    [Solution] 1.1 Setting up application and add hello world component

--- expense_tracker/static/src/expense_tracker.js
+++ expense_tracker/static/src/expense_tracker.js
@@ -1,1 +1,5 @@
-// Here code will come
+import { Component, xml } from "@odoo/owl";
+
+export class ExpenseTracker extends Component {
+    static template = xml`<h1>Hello World</h1>`;
+}
--- expense_tracker/static/src/main.js
+++ expense_tracker/static/src/main.js
@@ -1,1 +1,18 @@
-// Code will come here
+import { rpc } from "@web/core/network/rpc";
+import { App, EventBus } from "@odoo/owl";
+import { getTemplate } from "@web/core/templates";
+import { ExpenseTracker } from "./expense_tracker";
+
+owl.whenReady(async () => {
+    const bus = new EventBus();
+    const datetime = new Date();
+    const env = { bus, rpc, datetime };
+
+    const app = new App(ExpenseTracker, {
+        env,
+        getTemplate,
+        warnIfNoStaticProps: true, // !session.test_mode,
+        name: ExpenseTracker.constructor.name,
+    });
+    const root = await app.mount(document.body);
+});
