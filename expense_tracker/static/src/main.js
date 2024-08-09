a6b032a1d5d010af5a3c18ddc6b3752f9119dab1
Mohammed Shekha <msh@odoo.com>
2024-08-09 15:56:06 +0530

    [Solution] 1.15 Add translation feature

--- expense_tracker/static/src/main.js
+++ expense_tracker/static/src/main.js
@@ -8,11 +8,45 @@ owl.whenReady(async () => {
     const datetime = new Date();
     const env = { bus, rpc, datetime };
 
+    const translations = {};
+    const urlParams = new URLSearchParams(window.location.search);
+    const lang = urlParams.get('lang');
+    let translationBundle;
+    if (lang) {
+        const terms = {
+            fr_FR: {
+                "Home": "Maison",
+                "Description": "Description",
+                "Date:": "Date",
+                "Amount": "Montante",
+                "Category": "Catégorie",
+                "Categories": "Catégories",
+                "Tags": "Balises",
+                "Dashboard": "Tableau de bord",
+                "Quick Access": "Accès rapide",
+                "New Expense": "Nouvelle dépense",
+                "New Category": "Nouvelle catégorie",
+                "Expenses": "Dépenses",
+                "Total Amount:": "Montant total:",
+            }
+        };
+        Object.assign(translations, terms);
+        translationBundle = translations[lang];
+    }
+
+    const getTranslation = (str) => {
+        if (!translationBundle) {
+            return str;
+        }
+        return translationBundle[str] || str;
+    }
+
     const app = new App(ExpenseTracker, {
         env,
         getTemplate,
         warnIfNoStaticProps: true, // !session.test_mode,
         name: ExpenseTracker.constructor.name,
+        translateFn: getTranslation,
     });
     const root = await app.mount(document.body);
 });
